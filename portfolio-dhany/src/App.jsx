d7bf3798357ad431c3144771ef82082e7f041dc6
unknown <intech.bps@gmail.com>
2023-07-12 14:44:10 +0700

    Version 1

---
+++ portfolio-dhany/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+import Navbar from './components/Navbar'
+import Home from './pages/home/home'
+import About from './pages/about/about'
+import Portfolio from './pages/portfolio/portfolio'
+import Contact from './pages/contact/contact'
+ 
+function App() {
+  return ( 
+  <BrowserRouter>
+    <Navbar/>  
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='portfolio' element={<Portfolio/>}/>
+      <Route path='contact' element={<Contact/>}/>
+    </Routes>  
+  <BrowserRouter/>;
+  );
+}
+
+export default App;
